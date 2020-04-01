652eab6de93829545bd88cd22edaea4f0ffb6462
Taevon Jordan <nude.plus@Taevons-MBP.home>
2020-04-01 12:02:15 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
+class CashRegister
+  attr_accessor :total
 
+  def initialize(total= 0, discount= 20)
+    @total = total
+    @employee_discount = discount
+  end
+
+  def discount
+    @employee_discount
+  end
+
+  def add_item(title, price)
+    self.total += 1
+  end
+
+end
